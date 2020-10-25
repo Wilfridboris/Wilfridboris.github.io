50427c2f589c54e870e6c57d360a36e9c3ffe606
willfrid boris <boriskwayep35@yahoo.com>
2020-10-25 01:02:21 -0400

    :tada:

--- base.js
+++ base.js
@@ -1,7 +1,10 @@
 
 //INITIATE MY MODULE
 (function(){
-// MY HEAD MODULE
+   
+
+  
+// MY base module index
 var app=angular.module('base',[]);
 app.directive('headBase',()=>{
     return{
@@ -11,10 +14,37 @@ app.directive('headBase',()=>{
 
 
 });
+//my menu panel control
+
+app.controller("leftMenu",function(){
+    /**left menu state change */
+   this.change=function(e){
+       //e.preventDefault();
+       se=document.querySelectorAll('a.selected');
+        se[0].removeAttribute('class');
+        e.srcElement.setAttribute('class','selected');
+        
+            
+       };
+       
+
+});
+app.controller("subscript",function(){
+    this.hide=function(){
+    cl=document.getElementById('page');
+    cl.style.display="none";
+    
+    };
+    
+    
+    
+    });
 app.directive('controlPanel',()=>{
     return{
         'restrict':'E',
         'templateUrl':'/includes/controlPanel.html'
     }
-})
-})()
+});
+
+})();  
+
